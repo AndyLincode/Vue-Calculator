b2a5cc4c88799a29aeb9d873c30df9574c4b8382
Andy Ln <ld130101@gmail.com>
2023-01-04 16:05:23 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import Calculator from './components/Calculator.vue';
+import '@/assets/index.css'
+
+</script>
+
+<template>
+  <h1 class=" text-4xl">Vue Calculator</h1>
+  <div class=" bg-[#31475e] p-3 mt-3">
+    <Calculator></Calculator>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
